ad30679a7df89d93c531417dce60512c2937f3d3
Tulesha <tulesha_xochet_pit@mail.ru>
2020-06-25 18:14:12 +0300

    Implement service

---
+++ WCF_Chat/ServiceChat.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Chat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        private List<ServerUser> users = new List<ServerUser>();
+        private int nextID = 1;
+
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                ID = nextID,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextID++;
+
+            SendMsg(user.Name + "joined the chat!", 0);
+            users.Add(user);
+            return user.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMsg(user.Name + "left the chat", 0);
+            }
+        }
+
+        public void SendMsg(string msg, int id)
+        {
+            foreach (var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+
+                var user = users.FirstOrDefault(i => i.ID == id);
+                if (user != null)
+                {
+                    answer += ": " + user.Name + " ";
+                }
+                answer += msg;
+
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(answer);
+            }
+        }
+    }
+}
